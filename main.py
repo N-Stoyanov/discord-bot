77f881f2c2bd2a3d1d17a27d1dcf8b9c56efd49a
Nick <nickstoyanov789@gmail.com>
2025-08-31 23:02:03 -0500

    Update main.py
    Added a simple shutdown feature.

--- main.py
+++ main.py
@@ -17,14 +17,19 @@ class Client(discord.Client):
             return
         if message.content.startswith('hello'):
             await message.channel.send(f'Hi there {message.author}')
+        if message.content == "!shutdown":
+            await message.channel.send("Shutting down...")
+            await client.close()    
     
     async def on_reaction_add(self, reaction, user):
         await reaction.message.channel.send('You reacted')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-
 client.run(TOKEN)
+
+
 
